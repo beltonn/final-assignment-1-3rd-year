6001e8ded31baaed43162f046c5625e2fd7504d2
beltonn <32432418+beltonn@users.noreply.github.com>
2017-10-18 19:43:36 +0100

    Added to Lowest Common Ancestor test

--- BinaryTreeTest.java
+++ BinaryTreeTest.java
@@ -16,13 +16,19 @@ public class BinaryTreeTest {
         Node node3 = new Node(3);
         Node node4 = new Node(4);
         Node node5 = new Node(5);
+        Node node6 = new Node(6);
         root.setLeft(node2);
         root.setRight(node3);
         node2.setLeft(node4);
         node2.setRight(node5);
-    
-        Node lca = bt.lowestCommonAncestor(root, node4, node2);
-        assertEquals("The lowest common ancestor:", root, lca);   
+        node5.setLeft(node6);
+               
+        
+        Node lca = bt.lowestCommonAncestor(root, node4, node5);
+        assertEquals("The lowest common ancestor:", node2, lca);
+        
+        lca = bt.lowestCommonAncestor(root, node5, node6);
+        assertEquals("The lowest common ancestor:", node2, lca);
         
     }
 
